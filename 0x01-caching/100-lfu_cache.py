b9a4242580e1bed4ea3464cddb4f8e48c64e111d
xLion <larhnimikit@gmail.com>
2024-04-03 10:11:36 +0000

    the caching system ..

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,47 +1,59 @@
-#!/usr/bin/python3
-""" LFUCaching module
-"""
-from base_caching import BaseCaching
+#!/usr/bin/env python3
+"""a class LFUCache that inherits from BaseCaching and is a caching system"""
+
+from collections import defaultdict
+from functools import cmp_to_key
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ Defines a caching system using LFU algorithm
-    """
+    """ Defines the LFUCache class """
 
     def __init__(self):
-        """ Initialize LFUCache
-        """
+        """ Initializes the LFUCache instance """
         super().__init__()
-        self.frequency = {}
+        self.frequency = defaultdict(int)
+        self.order_used = []
 
     def put(self, key, item):
-        """ Add an item to the cache using LFU algorithm
-        """
+        """ Adds an item to the cache using LFU algorithm """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                min_freq = min(self.frequency.values())
-                lfu_keys = [k for k, v in self.frequency.items()
-                            if v == min_freq]
-
-                if len(lfu_keys) > 1:
-                    lru_key = min(lfu_keys, key=lambda k: self.order.index(k))
-                    del self.cache_data[lru_key]
-                    del self.frequency[lru_key]
-                    print("DISCARD: {}".format(lru_key))
-                else:
-                    lfu_key = lfu_keys[0]
-                    del self.cache_data[lfu_key]
-                    del self.frequency[lfu_key]
-                    print("DISCARD: {}".format(lfu_key))
-
+                self._discard_least_frequent()
             self.cache_data[key] = item
-            self.frequency[key] = self.frequency.get(key, 0) + 1
+            self.frequency[key] += 1
+            self.order_used.append(key)
 
     def get(self, key):
-        """ Get an item from the cache by key
-        """
+        """ Retrieves an item from the cache """
         if key is not None and key in self.cache_data:
+            # Update frequency and order_used list
             self.frequency[key] += 1
+            self.order_used.remove(key)
+            self.order_used.append(key)
             return self.cache_data[key]
+        return None
+
+    def _discard_least_frequent(self):
+        """ Discards the least frequent used item(s) """
+        min_frequency = min(self.frequency.values())
+        least_frequent_keys = [key for key, freq in self.frequency.items(
+            ) if freq == min_frequency]
+        if len(least_frequent_keys) == 1:
+            discarded_key = least_frequent_keys[0]
         else:
-            return None
+            discarded_key = min(
+                    least_frequent_keys, key=lambda k:
+                    self.order_used.index(k)
+                    )
+        del self.cache_data[discarded_key]
+        del self.frequency[discarded_key]
+        self.order_used.remove(discarded_key)
+        print(f"DISCARD: {discarded_key}")
+
+    def _compare(self, x, y):
+        """ Custom comparison """
+        if self.frequency[x] != self.frequency[y]:
+            return self.frequency[x] - self.frequency[y]
+        return self.order_used.index(y) - self.order_used.index(x)
